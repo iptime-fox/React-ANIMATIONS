1aa7cb938528a280b0c4690bbba7cbfc258b26d7
iptime-fox <dudndi826@gmail.com>
2023-07-11 19:04:30 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import { styled } from 'styled-components';
+import { motion } from 'framer-motion';
+
+const Wrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Box = styled(motion.div)`
+  width: 200px;
+  height: 200px;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 40px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const boxVariants = {
+  hover: { scale: 1.5, rotateZ: 90 },
+  click: { scale: 1, borderRadius: '100px' },
+};
+
+function App() {
+  return (
+    <Wrapper>
+      <Box
+        drag
+        whileDrag={{ backgroundColor: 'rgb(120, 193, 243)' }}
+        variants={boxVariants}
+        whileHover='hover'
+        whileTap='click'></Box>
+    </Wrapper>
+  );
+}
+
+export default App;
